fbe41d358cc6be1289f1a4f1752a8c28c4aec4e6
frdrckaman <frdrckaman@gmail.com>
2023-05-14 09:40:01 +0300

    menus, template segmentation

---
+++ somans_dashboard/views/software_view.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic.base import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = f"somans_dashboard/bootstrap/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # site_profile = Site.objects.get_current()
+        # context.update(
+        #     installed_apps=settings.INSTALLED_APPS,
+        #     # s_id=site_profile.id
+        # )
+        return context
+
+    # @method_decorator(login_required)
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
+
