9512292659fb1aa6a3265bdeb08950424b8f0da7
Amani, Fredrick F <fredrick.amani@stanbic.co.tz>
2023-07-20 10:28:25 +0300

    approval request

--- somans_dashboard/views/approve_software_request.py
+++ somans_dashboard/views/approve_software_request.py
@@ -1,19 +1,22 @@
 from django.shortcuts import redirect
+from django.urls import reverse
+
 from somans_dashboard.models import ApproveSoftware
 
 
-def send_approval_request(request):
+def send_approval_request(request, url=None):
     if request.method == 'POST':
-        print(request.POST.get("app_name"))
-        print(request.POST.get("devices_permitted"))
-        print(request.POST.get("reasons"))
-        # ticket, created = ApproveSoftware.objects.get_or_create(
-        #     product_name=request.POST.get("app_name"),
-        #     devices_permitted=request.POST.get("devices_permitted"),
-        #     reasons=request.POST.get("reasons")
-        # )
-        # if created:
-        #     print('Oooooooooooooooooooooooooooook')
-        # else:
-        #     print('Nooooooooooooooooooooooooooooo')
-    return redirect('somans_dashboard:nw-app-ls-svr')
+        ticket, created = ApproveSoftware.objects.get_or_create(
+            product_name=request.POST.get("app_name"),
+            devices_permitted=request.POST.get("devices_permitted"),
+            reasons=request.POST.get("reasons")
+        )
+        if created:
+            res = 'success'
+            message = 'Request submitted successful'
+        else:
+            res = 'error'
+            message = 'Error occurred while your request,please check your inputs and try again'
+        notification = res + '&message=' + message
+        url = "?response=".join([reverse('somans_dashboard:nw-app-ls-svr'), notification])
+    return redirect(url)
