f46ed402678b77d590595ce0833253185ebf178d
Amani, Fredrick F <fredrick.amani@stanbic.co.tz>
2023-08-03 11:17:40 +0300

    finservices icon status

--- somans_dashboard/templatetags/idap_fins.py
+++ somans_dashboard/templatetags/idap_fins.py
@@ -15,19 +15,27 @@ def FinStatusIcon1(context, counter):
     ls_status1 = ls1[0]
 
     if counter in [9, 11, 20]:
-        theme1 = 'bg-theme-12'
-        icon1 = 'thumbs-up'
+        theme1 = 'bg-dark'
+        icon1 = 'chevron-down'
+        icon_text1 = 'text-primary'
+        sv_status1 = 'Down'
     else:
         if ls_status1[counter] == 'UP':
-            theme1 = 'bg-theme-9'
-            icon1 = 'thumbs-up'
+            theme1 = 'bg-success'
+            icon1 = 'chevron-up'
+            icon_text1 = 'text-primary'
+            sv_status1 = 'Up'
         else:
-            theme1 = 'bg-theme-6'
-            icon1 = 'alert-triangle'
+            theme1 = 'bg-danger'
+            icon1 = 'chevron-down'
+            sv_status1 = 'Down'
+            icon_text1 = 'text-danger'
 
     return dict(
         theme=theme1,
         icon=icon1,
+        icon_text=icon_text1,
+        sv_status=sv_status1,
     )
 
 
@@ -43,15 +51,21 @@ def FinStatusIcon2(context, counter):
     ls_status2 = ls2[0]
 
     if ls_status2[counter] == 'UP':
-        theme2 = 'bg-theme-9'
-        icon2 = 'thumbs-up'
+        theme2 = 'bg-success'
+        icon2 = 'chevron-up'
+        icon_text2 = 'text-primary'
+        sv_status2 = 'Up'
     else:
-        theme2 = 'bg-theme-6'
-        icon2 = 'alert-triangle'
+        theme2 = 'bg-danger'
+        icon2 = 'chevron-down'
+        icon_text2 = 'text-danger'
+        sv_status2 = 'Down'
 
     return dict(
         theme=theme2,
         icon=icon2,
+        icon_text=icon_text2,
+        sv_status=sv_status2
     )
 
 
