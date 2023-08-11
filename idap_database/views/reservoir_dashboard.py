764ee625615574d317b64565064463667ba3c32e
Amani, Fredrick F <fredrick.amani@stanbic.co.tz>
2023-08-11 16:41:03 +0300

    update

--- idap_database/views/reservoir_dashboard.py
+++ idap_database/views/reservoir_dashboard.py
@@ -1,8 +1,8 @@
 from django.views.generic.base import TemplateView
-from somans_dashboard.view_mixins import SoftwareListboardView
+from somans_dashboard.view_mixins import SoftwareListboardView, IdapLoginMixin
 
 
-class ReservoirDashboardView(SoftwareListboardView, TemplateView):
+class ReservoirDashboardView(IdapLoginMixin, TemplateView):
     template_name = f"idap_database/bootstrap/reservoir_dashboard.html"
 
     def get_context_data(self, **kwargs):
