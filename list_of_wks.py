dd95c9b4647598da4cf73f688158ffa2dc12f172
a248080 <fredrick.amani@stanbic.co.tz>
2023-09-05 10:43:27 +0300

    list of workstations

--- list_of_wks.py
+++ list_of_wks.py
@@ -11,8 +11,10 @@ logVar = {'project_name': 'SOMANS', 'report_tag': 'ls_wks', 'status': 'Success',
 dfLog = pd.DataFrame(logVar, index=[0])
 
 try:
-    myQuery = pd.read_csv(env_mixin.SOMANS_LS_OF_SVRS_DATA)
-    df = myQuery
+    myQuery = pd.read_csv(env_mixin.SOMANS_LS_OF_WKS_DATA)
+    df = myQuery.rename(columns={'computer_ip_addess': 'computer_ip_address',
+                                 'distinguised_name_as_listed_in_active_directory': 'distinguised_name',
+                                 'username': 'user_name'})
     msg0 = 'List of workstation data fetched Successful'
     logger.info(msg0)
     dfLog['report_date'] = datetime.today().strftime('%Y-%m-%d')
@@ -20,14 +22,16 @@ try:
     dfLog['job_output'] = msg0
     dfLog.to_sql(env_mixin.IDAP_LOG_TBL, env_mixin.engine_idap, if_exists='append', index=False)
     try:
-        df.to_sql(env_mixin.SOMANS_LS_OF_SVRS, env_mixin.engine_idap, if_exists='append', index=False)
+        df.to_sql(env_mixin.SOMANS_LS_OF_WKS, env_mixin.engine_idap, if_exists='append', index=False)
         msg1 = 'List of workstation data inserted Successful'
         logger.info(msg1)
         dfLog['status'] = 'Success'
         dfLog['job_output'] = msg1
         dfLog.to_sql(env_mixin.IDAP_LOG_TBL, env_mixin.engine_idap, if_exists='append', index=False)
         try:
-            df.to_sql(env_mixin.SOMANS_LS_OF_SVRS_HIST, env_mixin.engine_idap, if_exists='append', index=False)
+            df['job_date'] = datetime.today().strftime('%Y-%m-%d')
+            df['job_timestamp'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+            df.to_sql(env_mixin.SOMANS_LS_OF_WKS_HIST, env_mixin.engine_idap, if_exists='append', index=False)
             msg2 = 'List workstation history software data Successful'
             logger.info(msg1)
             dfLog['status'] = 'Success'
