776dcb54de6148577e6208c7888c2b466432f337
a248080 <fredrick.amani@stanbic.co.tz>
2023-09-08 13:35:55 +0300

    cims update

--- cims.py
+++ cims.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import env_mixin
 
-engine = create_engine("oracle+cx_oracle://readonly:readonly@(DESCRIPTION=(SOURCE_ROUTE=YES)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=ytzocm3.tz.sbicdirectory.com)(port=1621))(ADDRESS=(PROTOCOL=TCP)(HOST=ytzoda8-scan2.tz.sbicdirectory.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ptz3cor.tz.sbicdirectory.com)))")
+engine = create_engine(env_mixin.ORA_CONN_STRING_DR)
 
 df = pd.read_sql_query("select trunc(sysdate -4) from DUAL", engine)
 
