93f33f7f5f268629bb492948b5d26e8eb4bc0388
Amani, Fredrick F <fredrick.amani@stanbic.co.tz>
2023-10-04 13:14:47 +0300

    update db connection sqlite to sqlserver

--- somans/settings.py
+++ somans/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 import environ
 import sqlalchemy
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +30,6 @@ DEBUG = env("DJANGO_DEBUG")
 
 APP_NAME = env.str("DJANGO_APP_NAME")
 SOMANS_SQLITE = env.str("SOMANS_SQLITE")
-SOMANS_ENGINE = sqlalchemy.create_engine(SOMANS_SQLITE)
 SOMANS_ADMIN = env.str("SOMANS_ADMIN")
 SOMANS_OPERATOR = env.str("SOMANS_OPERATOR")
 SOMANS_NEW_APP_SVR = env.str("SOMANS_NEW_APP_SVR")
@@ -53,6 +54,28 @@ FIN_SERVICES = env.str("FIN_SERVICES")
 
 IDAP_POS_DT_DIR = env.str("IDAP_POS_DT_DIR")
 IDAP_ADMIN = env.str("IDAP_ADMIN")
+IDAP_CLUSTER = env.str("IDAP_CLUSTER")
+IDAP_SERVER = env.str("IDAP_SERVER")
+IDAP_USER = env.str("IDAP_USER")
+IDAP_PWD = env.str("IDAP_PWD")
+IDAP_DB = env.str("IDAP_DB")
+IDAP_DB_PORT = env.str("IDAP_DB_PORT")
+IDAP_DB_DRIVER = env.str("IDAP_DB_DRIVER")
+IDAP_TDS_VERSION = env.str("IDAP_TDS_VERSION")
+
+RSS_DB = env.str("RSS_DB")
+RSS_DRIVER = env.str("RSS_DRIVER")
+RSS_ODBC_DRIVER = env.str("RSS_ODBC_DRIVER")
+RSS_INSTANCE = env.str("RSS_PR")
+RSS_LOGS = env.str("RSS_LOGS")
+RSS_PASSWORD = env.str("RSS_PASSWORD")
+RSS_PORT = env.str("RSS_PORT")
+RSS_SERVER = env.str("RSS_SERVER_PR01")
+RSS_TDS_VERSION = env.str("RSS_TDS_VERSION")
+RSS_USERNAME = env.str("RSS_USERNAME")
+RSS_SCHEMA = env.str("RSS_SCHEMA")
+RSS_LOG_TBL = env.str("RSS_LOG_TBL")
+RSVR_CLUSTER = env.str("RSVR_CLUSTER")
 
 ALLOWED_HOSTS = ['*']
 
@@ -111,10 +134,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'somans.wsgi.application'
 
+connection_string_idap = f"DRIVER={IDAP_DB_DRIVER};SERVER={IDAP_CLUSTER},{IDAP_DB_PORT};DATABASE={IDAP_DB};ENCRYPT=yes;TrustServerCertificate=Yes;MultiSubnetFailover=Yes;UID={IDAP_USER};PWD={IDAP_PWD}"
+connection_url_idap = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string_idap})
+engine_idap = create_engine(connection_url_idap)
+
+connection_string_rss = f"DRIVER={RSS_ODBC_DRIVER};SERVER={RSVR_CLUSTER};DATABASE={RSS_DB};ENCRYPT=yes;TrustServerCertificate=Yes;MultiSubnetFailover=Yes;UID={RSS_USERNAME};PWD={RSS_PASSWORD}"
+connection_url_rss = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string_rss})
+engine_rss = create_engine(connection_url_rss)
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 if env("DATABASE_SQLITE_ENABLED"):
+    SOMANS_ENGINE = sqlalchemy.create_engine(SOMANS_SQLITE)
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -123,6 +155,7 @@ if env("DATABASE_SQLITE_ENABLED"):
     }
 
 else:
+    SOMANS_ENGINE = engine_idap
     # DATABASES = {"default": env.db()}
     DATABASES = {
         'default': {
